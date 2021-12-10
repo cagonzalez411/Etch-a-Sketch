dd2ea04924feeb1793f3059eb89a408d12d10cdb
Carlos Gonzalez <cagonzalez411@gmail.com>
2021-12-10 00:39:29 -0500

    Added the fixed grid. Need to fix the  eventHandlers

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const gridSize = 16*16;
+let sizeofGrid = 16;
 const grid = document.querySelector('.grid');
-const gridResetBtn = document.querySelector('#resetButton')
 
-for (var i = 0; i < gridSize; i++) {
-    var div = document.createElement("div")
-    div.className = "gridDiv"
-    grid.appendChild(div)
+function createRows(gridSize) {
+    for (var i = 0; i < gridSize; i++) {
+        var div = document.createElement("div");
+        div.className = "row";
+        grid.appendChild(div);
+    }; 
+}
+
+function createCols(gridSize) {
+    let rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        for (let i = 0; i < gridSize; i++) {
+            var col = document.createElement("div");
+            col.className = "col";
+            row.appendChild(col);
+        }
+    }) 
 };
 
-const gridDivs = document.querySelectorAll('.gridDiv')
+function createGrid(sizeofGrid) {
+    createRows(sizeofGrid)
+    createCols(sizeofGrid)
+    
+    cols.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.classList.add('hovered');
+        })
+    });
+}
 
-gridDivs.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.classList.add('hovered');
-    })
-});
+let cols = document.querySelectorAll('.col')
+
+createGrid(sizeofGrid)
+const gridResetBtn = document.querySelector('#resetButton');
 
 gridResetBtn.addEventListener('click', () => {
-    gridDivs.forEach(item => {
+    cols.forEach(item => {
         item.classList.remove('hovered')
     });
+    let userSize = prompt("Enter the desired square size.")
+    createGrid(userSize)
 });
 
 
